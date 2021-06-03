9322f19b5b80f4d7e8f69d93857cfad9921fec0b
Maggie Zhao <LixiaZhao@yale.edu>
2021-06-03 13:19:44 -0400

    add U+200f to be replaced

--- src/main/java/edu/yale/library/jpegs2pdf/JpegPdfConcatImpl.java
+++ src/main/java/edu/yale/library/jpegs2pdf/JpegPdfConcatImpl.java
@@ -304,7 +304,8 @@ public class JpegPdfConcatImpl implements JpegPdfConcat {
 				.replace('\n', ' ')
 				.replace('\r', ' ')
 				.replace('\t', ' ')
-				.replaceAll("\\p{Cc}", "");
+				.replaceAll("\\p{Cc}", "")
+                                .replaceAll("U+200F", "");
 	}
 
 	public static final Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$",
